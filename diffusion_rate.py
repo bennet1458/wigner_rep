1b2819b73423cab1511fa76c15b5bcf46da5ae68
Bennet Wucherpfennig <bewu@uni-bremen.de>
2026-06-01 14:33:48 +0200

    Added a calculation for the diffusion rate.

---
+++ diffusion_rate.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def Lambda_x(rho, omega0, V, hbar, lam, eps_r):
+    return (4*np.pi**2/3) * (rho * omega0**2 * V**2) / (hbar * lam**3) * np.real(
+        (eps_r - 1) / (eps_r + 2)
+    )
+
+
+hbar = 1.054e-34
+
+omega0 = 50e3
+d = 50e-9
+M = 1.4e-19
+lam = 1550e-9
+n_L= 1.44+1j*2.5e-9
+
+eps_r = n_L**2
+print(eps_r)
+V = (4/3)*np.pi*(d/2)**3
+rho = M/V 
+
+print(Lambda_x(rho, omega0, V, hbar, lam, eps_r))
